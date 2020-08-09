9e20913de0a141cd2639d01d26acad34bb1eedf6
Sushant Srivastava <sushant.srivastav@gmail.com>
2020-08-09 17:42:34 +0530

    First cut

---
+++ src/mitra_varuna/patentprocess_ontology.clj
@@ -0,0 +1,7 @@
+(ns mitra-varuna.patentprocess-ontology
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/mitra_varuna/patentprocess_ontology_test.clj
@@ -0,0 +1,7 @@
+(ns mitra-varuna.patentprocess-ontology-test
+  (:require [clojure.test :refer :all]
+            [mitra-varuna.patentprocess-ontology :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
